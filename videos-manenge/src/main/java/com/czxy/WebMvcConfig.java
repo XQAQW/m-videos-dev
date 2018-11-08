af60cd09299083bda4d83e67ee334f85c20d68c6
mac <xqwQAQwq@163.com>
2018-11-08 17:31:32 +0800

    add WebMvcConfig配置类 继承 WebMvcConfigurerAdapter类 重写addResourceHandlers方法

---
+++ videos-manenge/src/main/java/com/czxy/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.czxy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                //资源映射
+        registry.addResourceHandler("/**")
+                //添加映射可以让外界访问
+                .addResourceLocations("classpath:/META-INF/RESOURCES/")
+                //所在的目录
+                .addResourceLocations("file:/usr/local/programing/WeChartProject/video_dev/videos_dev_image/");
+    }
+}
